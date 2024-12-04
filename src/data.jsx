4752311ca4ae98f097d91c92cd37814f868c79ac
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-12-04 15:50:49 -0500

    updated image routes

--- src/data.jsx
+++ src/data.jsx
@@ -3,7 +3,7 @@ export const articles = [{
   name: 'Creola Katherine Johnson',
   date: '11/12/20',
   title: 'On the Street in Brooklyn',
-  image: '../../public/blog-image-1.jpg', 
+  image: 'https://i.postimg.cc/xCvm1J7L/blog-image-1.jpg', 
   text: "Can you take me to utopia Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem suscipit fugiat tempore hic repellendus explicabo blanditiis ex commodi est, maximetium molestiae ipsum ducimus voluptate maiores! Architecto autem reprehenderit facilis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim dolores aliquam consequuntur ullam unde ut quos labore alias doloremque vitae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eos iure magni? Magni facere rem cum quos voluptates commodi sunt obcaecati consequatur deleniti at dicta sint repudiandae quam, nulla omnis?", 
   alt: "Walking in New York."
 }, {
@@ -11,7 +11,7 @@ export const articles = [{
   date: '11/11/20',
   title: 'Vintage in Vogue',
   accomplishment: 'discovery of Arctic ozone hole',
-  image: '../../public/blog-image-2.jpg', 
+  image: 'https://i.postimg.cc/j2R7wq0m/blog-image-2.jpg', 
   text: "Selfies sunt, placeholder text to demonstrate justified text alignment. Will you do the same for me? It's time to face the music I'm no longer your muse. Heard it's beautiful, be the judge and my girls gonna take a vote. I can feel a phoenix inside of me. Heaven is jealous of our love, angels are crying from up above. Yeah, you take me to utopia Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem suscipit fugiat tempore hic repellendus explicabo blanditiis ex commodi est, maxime accusantium molestiae ipsum ducimus voluptate maiores! Architecto autem reprehenderit facilis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim dolores aliquam consequuntur ullam unde ut quos labore alias doloremque vitae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eos iure magni? Magni facere rem cum quos voluptates commodi sunt obcaecati consequatur deleniti at dicta sint repudiandae quam, nulla omnis?", 
   alt: "Three people looking at something."
 }];
